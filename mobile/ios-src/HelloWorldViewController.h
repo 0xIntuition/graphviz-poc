fe64ce9b3c2329b3b12c16c79c64d642b4c3aec6
Simonas Karuzas <simonas@not.cat>
2024-04-12 13:27:45 +0300

    feat: open native view controller

---
+++ mobile/ios-src/HelloWorldViewController.h
@@ -0,0 +1,16 @@
+//
+//  HelloWorldViewController.h
+//  intuition_mobile
+//
+//  Created by Simonas Karuzas on 11/04/2024.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HelloWorldViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
